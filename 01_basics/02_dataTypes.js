9ce1d3183ace576fe2d140ff0e00daaff9769424
Eshankhandelwal <146695075+Eshankhandelwal@users.noreply.github.com>
2024-09-12 06:53:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+ 3)// we are using nodejs not browser
+
+console.log(3+3);
+console.log("Eshan Khandelwal");
+
+let name = "Eshan"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//number 2 to power 53
+//bigint
+//string ->""
+//boolean ->true/false
+//null ->standalone value
+//undefined ->
+//symbol ->unique
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
+console.log(typeof "Eshan");    //string
+console.log(typeof 21);         //number
+console.log(typeof false);      //boolean
